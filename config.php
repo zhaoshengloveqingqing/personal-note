6a28c932ec1701aeb03b4531b6b8ae6a2675b628
zhaoshengloveluqingqing <530053776@qq.com>
2016-02-05 09:45:49 +0800

    modify commit

--- config.php
+++ config.php
@@ -1,3 +1,4 @@
 1.澳瑞特酒店微信平台：账号：huijuan.ren@pinet.co 密码：pinet2014 微信号：orient2014(wxf7e8e5b41e0bff0b    8a27f50d89c95efef5156f8511ccdda3)
 2.松江账号：funstore1778     密码：setup201203
-3.安徽派尔:93695714@qq.com   CATHY8813 微信公众平台:wxf0c1d6198e106e87     6f963a608d889f18b90c20cd62d82e85
+3.安徽派尔:93695714@qq.com   CATHY8813 微信公众平台:wxf0c1d6198e106e87     6f963a608d889f18b90c20cd62d82e85 原始id：gh_10d71083930e
+4.http://60.174.235.164/bestpay/mobile/?c=wechat&orgid=gh_7e9d64997c41
